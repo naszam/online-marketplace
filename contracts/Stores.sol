abedecd079d79d541d601fd1708ba4d72bf1af21
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-01-08 11:24:40 +0000

    Update Stores.sol

--- contracts/Stores.sol
+++ contracts/Stores.sol
@@ -7,13 +7,15 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/lifecycle/Pausable";
+import "./Marketplace.sol";
 
-contract Stores is Ownable, Pausable {
+contract Stores is Ownable, Pausable, Marketplace {
 
   uint storeId = 0;
   mapping (uint => Store) private store;
   mapping (uint => Item) private item;
 
+ // using address owner as id?
   struct Store {
     uint id;
     string name;
@@ -25,37 +27,67 @@ contract Stores is Ownable, Pausable {
     uint sku;
     string name;
     uint price;
-    bool purchased;
     uint quantity;
     uint storeId;
+    bool purchased;
   }
 
   event StoreAdded(uint id);
   event StoreRemoved(uint id);
-  event BalanceWithdrawal(address owner, uint balance);
   event ItemAdded(uint sku, uint storeId);
-  event ItemRemoved(uint suk, uint storeId);
+  event ItemRemoved(uint sku, uint storeId);
   event ItemPurchased(uint sku, uint storeId);
 
   modifier onlyStoreOwner(uint storeId) {
     require();
     _;
   }
-  function AddStore(string _name, address _owner, uint _balance)
+
+  function addStore(string memory _name, address _owner, uint _balance)
     public
     onlyStoreOwner(storeId)
     whenNotPaused
-    returns (bool)
+    returns(bool)
   {
     store[id] = Store({id: storeId, name: _name, owner: _owner, balance: _balance});
-    emit StoreAdded(uint storeId);
+    emit StoreAdded(storeId);
     storeId += 1;
     return true;
   }
 
+  function removeStore(uint id)
+    public
+    onlyStoreOwner(storeId)
+    whenNotPaused
+    returns(bool)
+  {
+    emit StoreRemoved(id);
+  }
 
+  function addItem(string memory _name, uint price, uint quantity, uint storeId)
+    public
+    onlyStoreOwner(storeId)
+    whenNotPaused
+    returns(bool)
+  {
+    emit ItemAdded(sku, storeId);
+  }
 
+  function removeItem(uint sku)
+    public
+    onlyStoreOwner(storeId)
+    whenNotPaused
+    returns(bool)
+  {
+    emit ItemRemoved(sku, storeId);
+  }
 
-
+  function buyItem(uint sku)
+    public
+    whenNotPaused
+    returns(bool)
+ {
+   emit ItemPurchased(sku);
+ }
 
 }
