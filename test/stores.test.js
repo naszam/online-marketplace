1fb2940071bac6fd72b03239eb345efe977c46c0
naszam <nazzareno@nazzarenomassari.com>
2020-01-14 00:40:40 +0000

    Update test

--- test/stores.test.js
+++ test/stores.test.js
@@ -15,5 +15,10 @@ contract('Stores', function(accounts) {
     beforeEach(async () => {
       instance = await Stores.new()
     })
-    
+
+    it("owner should be added as Admin and able to add store owner", async () => {
+      await instance.isAdmin(owner,{from:owner})
+      const ownerAdded = await instance.addStoreOwner(storeOwner, {from:admin})
+      assert.equal(ownerAdded, storeOwner, 'only admin should be able to add store owners')
+    })
 })
