8e29486ca6b8aa67ab22232cde3dc4a2dd435c8d
naszam <nazzareno@nazzarenomassari.com>
2020-01-18 17:06:31 +0000

    Update Adminable.sol

--- contracts/Adminable.sol
+++ contracts/Adminable.sol
@@ -20,7 +20,7 @@ contract Adminable is Ownable, Pausable {
   }
 
   /// @notice Fallback function
-  /// Added so ether sent to this contract is reverted if contract fails
+  /// @dev Added so ether sent to this contract is reverted if contract fails
   function() external payable {
     revert();
   }
@@ -30,7 +30,10 @@ contract Adminable is Ownable, Pausable {
     require(isAdmin(msg.sender), "Caller is not an Admin");
     _;
   }
-
+  
+  /// @notice check if the address is an Admin
+  /// @param account address to check
+  /// @dev used in onlyAdmin() modifier
   function isAdmin(address account)
     public
     view
@@ -39,6 +42,8 @@ contract Adminable is Ownable, Pausable {
     return admins[account];
   }
 
+  /// @notice add a new Admin
+  /// @param account address of the new Admin
   function addAdmin(address account)
     public
     whenNotPaused()
@@ -50,6 +55,8 @@ contract Adminable is Ownable, Pausable {
     return true;
   }
 
+  /// @notice remove an Admin
+  /// @param account admin address to remove 
   function removeAdmin(address account)
     public
     whenNotPaused()
